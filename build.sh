3a2657dffdf3baf60c81171d66a5de6d483b1501
Nathan Dorsey <421420+glycerine102@users.noreply.github.com>
2024-07-09 10:27:12 -0400

    Install packages needed for building alacritty

--- build.sh
+++ build.sh
@@ -4,20 +4,19 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+programming_packages=(
+  "cmake"
+  "freetype-devel"
+  "fontconfig-devel"
+  "libxcb-devel"
+  "libxkbcommon-devel"
+  "g++"
+)
 
-### Install packages
+packages=(
+  ${programming_packages[@]}
+)
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
+rpm-ostree install ${packages[@]}
 
 systemctl enable podman.socket
